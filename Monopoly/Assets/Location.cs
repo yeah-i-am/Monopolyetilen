9fb35aaa4d58c60f449a8b49bd99c9650a8ac5dc
EvgeniiVetoshkin <AA2892@student.jamk.fi>
2021-04-17 22:26:53 +0300

    NO

---
+++ Monopoly/Assets/Location.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+    public Transform positionPoint;
+
+    void Start()
+    {
+        //GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<Transform>().;
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
