59c2d7e88974f27909cb8ee20af085a08fa0377d
Eugene <peugene2406@gmail.com>
2021-04-18 19:03:56 +0300

    kamera klass

--- Monopoly/Assets/Scripts/CameraController.cs
+++ Monopoly/Assets/Scripts/CameraController.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class CameraController : MonoBehaviour
 {
     private Vector3 velocity = Vector3.zero;
-    private Vector3 cameraVelocity = Vector3.zero;
+    private Vector3 zoomVelocity = Vector3.zero;
+    private Vector3 lookAtPoint;
+    private float oldMousePosX = 0;
+    private float rotation = 0;
+    private float eah = 1 / 50;
 
     void Start()
     {
+        float c = zoomDespeed;
+
+        for (int i = 0; i < 50; i++)
+        {
+            eah += c / 50;
+            c *= zoomDespeed;
+        }
     }
 
     Vector3 GetInputTranslationDirection()
@@ -37,15 +49,18 @@ public class CameraController : MonoBehaviour
 
     float GetInputRotation()
     {
-        float rotaion = 0;
-
         if (Input.GetKey(KeyCode.Q))
-            rotaion += 1f;
+            rotation += rotationKeysSpeed;
 
         if (Input.GetKey(KeyCode.E))
-            rotaion -= 1f;
+            rotation -= rotationKeysSpeed;
 
-        return rotaion;
+        if (Input.GetKey(KeyCode.Mouse1))
+            rotation += (Input.mousePosition.x - oldMousePosX) * rotationMouseSpeed;
+
+        oldMousePosX = Input.mousePosition.x;
+
+        return rotation;
     }
 
     Vector3 GetInputCameraTranslationDirection()
@@ -64,19 +79,45 @@ public class CameraController : MonoBehaviour
         var sqrLength = velocity.sqrMagnitude;
 
         if (sqrLength > movementSpeed * movementSpeed)
-            velocity *= movementSpeed / Mathf.Sqrt(sqrLength);
+            velocity *= movementSpeed / math.sqrt(sqrLength);
 
         velocity *= movementDeaccseleration;
-        cameraVelocity *= zoomDespeed;
+        zoomVelocity *= zoomDespeed;
+
+        RaycastHit hit;
+
+        bool deskHit = Physics.Raycast(transform.position, transform.forward, out hit);
+
+        /* надо ещё запретить выходить за границы доски
+         * но это в другой раз */
+
+        lookAtPoint = hit.point;
     }
 
     private void Update()
     {
-        transform.Translate(velocity * Time.deltaTime);
-        transform.Rotate(new Vector3(0, rotaionSpeed * GetInputRotation() * Time.deltaTime, 0));
+        transform.Translate(Quaternion.Euler(-transform.rotation.eulerAngles.x, 0, 0) * velocity * Time.deltaTime);
 
-        cameraVelocity += GetInputCameraTranslationDirection() * zoomSpeed;
-        Camera.transform.Translate(cameraVelocity * Time.deltaTime);
+        if (smoothRotation)
+        {
+            transform.RotateAround(lookAtPoint, Vector3.up, GetInputRotation() * Time.deltaTime);
+            rotation *= (1f - Time.deltaTime);
+        }
+        else
+        {
+            transform.RotateAround(lookAtPoint, Vector3.up, GetInputRotation());
+            rotation = 0;
+        }
+
+        /* Zoom zoom */
+        zoomVelocity += GetInputCameraTranslationDirection() * zoomSpeed;
+        
+        var newDist = Vector3.Distance(lookAtPoint, transform.position) - zoomVelocity.z * eah;
+
+        if (newDist < minZoomDist || newDist > maxZoomDist)
+            zoomVelocity -= GetInputCameraTranslationDirection() * zoomSpeed;
+
+        transform.Translate(zoomVelocity * Time.deltaTime);
     }
 
     [Header("Movement Settings")]
@@ -90,17 +131,26 @@ public class CameraController : MonoBehaviour
     public float movementDeaccseleration = 0.9f;
 
     [Header("Zoom Settings")]
-    [Tooltip("Yes, that camera")]
-    public GameObject Camera;
-
     [Tooltip("zoom zoom")]
     public float zoomSpeed = 3f;
 
     [Tooltip("no zoom zoom"), Range(0.001f, 0.999f)]
     public float zoomDespeed = 0.9f;
 
+    [Tooltip("max zoom distance")]
+    public float maxZoomDist = 30f;
+
+    [Tooltip("min zoom distance")]
+    public float minZoomDist = 3f;
+
     [Header("Rotation Settings")]
-    [Tooltip("speed but for rotation")]
-    public float rotaionSpeed = 30f;
+    [Tooltip("speed but for rotation for mouse")]
+    public float rotationMouseSpeed = 30f;
+
+    [Tooltip("speed but for rotation for keys")]
+    public float rotationKeysSpeed = 1f;
+
+    [Tooltip("shit")]
+    public bool smoothRotation = false;
 
 }
